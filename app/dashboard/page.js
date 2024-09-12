8f81a0ef17a966afb6b68e70f0db30df342525c8
Jeff Sokol <JeffSokolDev@gmail.com>
2024-09-12 18:58:42 -0400

    SQL data retrieval

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,12 +1,29 @@
+import { getServerSideTodos, getCustomers } from "../lib/data"
+
+let data = await getServerSideTodos();
+let customers = await getCustomers();
+
 export default async function Page() {
-    let res = await fetch('https://jsonplaceholder.typicode.com/todos');
-    let data = await res.json()
+    
+    // console.log('*********************')
+    console.log(data)
+    // let res = await fetch('https://jsonplaceholder.typicode.com/todos');
+    // let data = await res.json()
     return (
+        <div>
         <ul>
             {data.map((todo) => (
-                <li key={todo.id}>
-                    {todo.id}-{todo.title}</li>
+                <li key={todo.name}>
+                    {todo.name}-{todo.country}</li>
             ))}
         </ul>
+        <ul>
+            {customers.map((cust) =>(
+                <li key={cust.id}>
+                    {cust.id}-{cust.name}
+                </li>
+            ))}
+        </ul>
+        </div>
     )
 }
--- app/lib/data.jsx
+++ app/lib/data.jsx
@@ -1,14 +1,35 @@
-'use client'
-import { useState, useEffect } from "react";
-
+import { sql } from "@vercel/postgres";
 export async function getServerSideTodos() {
-    const [todos, setTodos] = useState([]);
-
-    useEffect(() =>{
-        const fetchTodos = async () => {
-        const res = await fetch('https://jsonplaceholder.typicode.com/todos');
-        const data = await res.json();
-    };
-    fetchTodos();
-    }, []);
+// THIS WORKS TO RETRIEVE WHISKEY DATA!! DON'T SCREW WITH
+    let res = await fetch('https://whiskyhunter.net/api/distilleries_info/');
+    let data = await res.json()
+    return data;
 }
+
+export async function getCustomers() {
+        let res = await sql`SELECT * FROM customers`;
+        let customers =  res.rows
+        return customers;
+    }
+
+// //BELOW CODE WORKS IN EXAMPLE
+// export async function fetchInvoicesPages(query) {
+//     try {
+//         const count = await sql`SELECT COUNT(*)
+//       FROM invoices
+//       JOIN customers ON invoices.customer_id = customers.id
+//       WHERE
+//         customers.name ILIKE ${`%${query}%`} OR
+//         customers.email ILIKE ${`%${query}%`} OR
+//         invoices.amount::text ILIKE ${`%${query}%`} OR
+//         invoices.date::text ILIKE ${`%${query}%`} OR
+//         invoices.status ILIKE ${`%${query}%`}
+//     `;
+
+//         const totalPages = Math.ceil(Number(count.rows[0].count) / ITEMS_PER_PAGE);
+//         return totalPages;
+//     } catch (error) {
+//         console.error('Database Error:', error);
+//         throw new Error('Failed to fetch total number of invoices.');
+//     }
+// }
